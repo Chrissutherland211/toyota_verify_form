345a06c83bb49bc660db3658c449af953ba98bf4
topdeveloperpcp <brightsun90211@gmail.com>
2020-04-04 01:54:52 +0500

    inprogress

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './views';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './state/store';
+import ThemeSetting from './utilities/theme/index';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+
+const theme = createMuiTheme(ThemeSetting)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ src/utilities/theme/index.js
@@ -0,0 +1,26 @@
+export default {
+    palette: {
+        primary: {
+            light: '#ffff52',
+            main: '#ffd300',
+            dark: '#c7a200',
+            contrastText: '#000',
+        },
+        secondary: {
+            light: '#ff7539',
+            main: '#ff3d00',
+            dark: '#c30000',
+            contrastText: '#fafafa',
+        },
+        table: {
+            header: '#eaeaea',
+            main: '#ff3d00',
+            dark: '#c30000',
+            contrastText: '#fafafa',
+        },
+        spinner: {
+            color: '#009688',
+        }
+    },
+}
+  
